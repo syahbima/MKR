2458be7fb4ba2f371d11d33281e84abb05a6afc4
syahbima <syahbimaa@gmail.com>
2019-08-05 04:24:43 +0000

    add time indicator to approximate runing time

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 import numpy as np
 from data_loader import load_data
 from train import train
@@ -10,14 +11,14 @@ parser = argparse.ArgumentParser()
 
 # movie
 parser.add_argument('--dataset', type=str, default='movie', help='which dataset to use')
-parser.add_argument('--n_epochs', type=int, default=30, help='the number of epochs')
+parser.add_argument('--n_epochs', type=int, default=5, help='the number of epochs')
 parser.add_argument('--dim', type=int, default=8, help='dimension of user and entity embeddings')
-parser.add_argument('--L', type=int, default=2, help='number of low layers')
-parser.add_argument('--H', type=int, default=2, help='number of high layers')
+parser.add_argument('--L', type=int, default=1, help='number of low layers')
+parser.add_argument('--H', type=int, default=1, help='number of high layers')
 parser.add_argument('--batch_size', type=int, default=4096, help='batch size')
 parser.add_argument('--l2_weight', type=float, default=1e-6, help='weight of l2 regularization')
-parser.add_argument('--lr_rs', type=float, default=6.25e-05, help='learning rate of RS task')
-parser.add_argument('--lr_kge', type=float, default=6.25e-05, help='learning rate of KGE task')
+parser.add_argument('--lr_rs', type=float, default=1e-06, help='learning rate of RS task')
+parser.add_argument('--lr_kge', type=float, default=1e-06, help='learning rate of KGE task')
 parser.add_argument('--kge_interval', type=int, default=3, help='training interval of KGE task')
 
 '''
@@ -52,5 +53,15 @@ show_loss = False
 show_topk = True
 
 args = parser.parse_args()
+
+start = time.time()
 data = load_data(args)
+end = time.time()
+
+print("Loading data time : ", end-start)
+
+start = time.time()
 train(args, data, show_loss, show_topk)
+end = time.time()
+
+print("Training time : ", end-start)
